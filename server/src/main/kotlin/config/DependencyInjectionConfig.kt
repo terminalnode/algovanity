9b629fc64e03ef43e2e503f72f0eccf8f688c559
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2022-06-13 22:30:42 +0200

    add properties file through koin

--- server/src/main/kotlin/config/DependencyInjectionConfig.kt
+++ server/src/main/kotlin/config/DependencyInjectionConfig.kt
@@ -4,12 +4,18 @@ import algo.terminal.algovanity.server.service.address.addressModule
 import algo.terminal.algovanity.server.service.generator.generatorModule
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import org.koin.environmentProperties
+import org.koin.fileProperties
 import org.koin.ktor.plugin.Koin
 import org.koin.logger.slf4jLogger
 
 fun Application.configureDependencyInjection() {
 	install(Koin) {
 		slf4jLogger()
+
+		fileProperties(fileName = "/algovanity.properties")
+		environmentProperties()
+
 		modules(
 			addressModule,
 			generatorModule,
